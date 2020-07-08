65f84e3ffbc963c9fa926daf4f7875d15db4ed55
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2020-07-08 12:58:27 +0600

    update version of deprecate module

--- ocd/deprecate.py
+++ ocd/deprecate.py
@@ -5,7 +5,7 @@
 __author__ = 'Md Jahidul Hamid <jahidulhamid@yahoo.com>'
 __copyright__ = 'Copyright © Md Jahidul Hamid <https://github.com/neurobin/>'
 __license__ = '[BSD](http://www.opensource.org/licenses/bsd-license.php)'
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 
 import inspect
